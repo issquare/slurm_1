8666266583221fd6f9216c493cc2afc4b1d44f8b
GitHub Action <action@github.com>
2025-11-24 17:46:17 +0000

    style: Auto-format with Black

--- dz_5.2_testing/test_dz_is_valid.py
+++ dz_5.2_testing/test_dz_is_valid.py
@@ -2,12 +2,12 @@ from dz_is_valid import is_valid
 
 
 def test_is_valid():
-    assert is_valid('()')
-    assert is_valid('[]')
-    assert is_valid('{}')
+    assert is_valid("()")
+    assert is_valid("[]")
+    assert is_valid("{}")
 
 
 def test_is_not_valid():
-    assert not is_valid('())')
-    assert not is_valid('[]]')
-    assert not is_valid('{}}')
+    assert not is_valid("())")
+    assert not is_valid("[]]")
+    assert not is_valid("{}}")
--- dz_5.2_testing/test_zadacha_1.py
+++ dz_5.2_testing/test_zadacha_1.py
@@ -5,4 +5,4 @@ def test_matrix(capfd):
     matrix()
     out, err = capfd.readouterr()
 
-    assert out == '12345\n678910\n1112131415\n1617181920\n2122232425\n'
+    assert out == "12345\n678910\n1112131415\n1617181920\n2122232425\n"
